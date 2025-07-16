e7685f4600dc365ae1bfd99612e04b5531b9101f
Juraj Martinka <jumarko@gmail.com>
2025-07-16 23:46:55 +0200

    Introduction - exercise 1: display upper right half of multiplication table (good version).
    This shows how easy it is to modify the program because it has good structure.

---
+++ 01-introduction/problem1-multiplication_good_ur.cbl
@@ -0,0 +1,25 @@
+      *> Attempt to modify the bad version of the program
+      *> to print the upper-right triangular half of the
+      *> multiplication table.
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. MULTIPLICATION-GOOD-UPPER-RIGHT.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       77 LINE-NO PIC 99.
+       77 COL-NO PIC 99.
+       01 PRINT-LINE.
+           02 NUM OCCURS 10 PIC ZZZ9.
+
+       PROCEDURE DIVISION.
+       PSTART.
+           PERFORM PLINE VARYING LINE-NO
+                   FROM 1 BY 1 UNTIL LINE-NO > 10
+           STOP RUN.
+       PLINE.
+           MOVE SPACES TO PRINT-LINE.
+           PERFORM PNUM VARYING COL-NO
+                   FROM LINE-NO BY 1 UNTIL COL-NO > 10.
+           DISPLAY PRINT-LINE.
+       PNUM.
+           MULTIPLY LINE-NO BY COL-NO GIVING NUM(COl-NO).
